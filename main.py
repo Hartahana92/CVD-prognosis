64bb7aab2bdce141014def8e255c5e726c96f0bd
Hartahana92 <kshsochi2014@gmail.com>
2021-11-04 20:48:05 +0300

    main

---
+++ main.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy import stats
+from sklearn.ensemble import RandomForestClassifier, BaggingClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import make_circles
+from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
+import pickle
+plt.style.use('ggplot')
+plt.rcParams['figure.figsize'] = 10, 6
+from sklearn.metrics import roc_auc_score
+from sklearn import preprocessing
+from sklearn import metrics
+st.title("Предсказательная модель оценки развития сердечно-сосудистых заболеваний")
+st.write("Вероятность развития ССЗ")
+data = st.file_uploader("Загрузите файл")
+if data is not None:
+    data = pd.read_excel(data)
+    loaded_model = pickle.load(open('finalized_model.pkl', 'rb'))
+    st.write(data)
+    a=loaded_model.predict_proba(data)
+    b=float(a[:,1])*100
+    st.write('вероятность CCЗ = ', round(b,2), '%')
+    if b>=70:
+        loaded_model2 = pickle.load(open('finalized_model_2.pkl', 'rb'))
+        c=loaded_model2.predict_proba(data)
+        d=float(c[:,1])*100
+        st.write('вероятность развития HTA = ', round(d,2), '%')
